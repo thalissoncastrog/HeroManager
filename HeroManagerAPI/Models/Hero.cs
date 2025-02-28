723f9d67222c98f3afc8d2cbc85c4f373fa927d4
Thalisson Castro <adaothalisson@hotmail.com>
2025-02-27 23:03:15 -0300

    created hero model

--- HeroManagerAPI/Models/Hero.cs
+++ HeroManagerAPI/Models/Hero.cs
@@ -1,6 +1,30 @@
-﻿namespace HeroManagerAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeroManagerAPI.Models
 {
+    [Table("Heroes")]
     public class Hero
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string HeroName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public float Height { get; set; }
+
+        public ICollection<HeroSuperPowers> HeroSuperPowers { get; set; }
+
     }
 }
