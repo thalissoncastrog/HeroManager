de7e5c3c51f8087f73c9bc51311e3c8de5ae8eec
Thalisson Castro <adaothalisson@hotmail.com>
2025-02-27 23:17:00 -0300

    created superpower model

--- HeroManagerAPI/Models/SuperPower.cs
+++ HeroManagerAPI/Models/SuperPower.cs
@@ -1,6 +1,24 @@
-﻿namespace HeroManagerAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeroManagerAPI.Models
 {
+    [Table("SuperPowers")]
     public class SuperPower
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Superpower { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string? Description { get; set; }
+
+        public ICollection<HeroSuperPowers> HeroSuperPowers { get; set; } = [];
     }
 }
