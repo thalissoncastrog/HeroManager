24ff7d205b744d52d988080d76df3733de879d2c
Thalisson Castro <adaothalisson@hotmail.com>
2025-02-27 23:21:08 -0300

    created HeroSuperPowers model

--- HeroManagerAPI/Models/HeroSuperPowers.cs
+++ HeroManagerAPI/Models/HeroSuperPowers.cs
@@ -1,6 +1,18 @@
-﻿namespace HeroManagerAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeroManagerAPI.Models
 {
+    [Table("HeroesSuperPowers")]
     public class HeroSuperPowers
     {
+        [Key]
+        public int HeroId { get; set; }
+        public Hero Hero { get; set; }
+
+        [Key]
+        public int SuperPowerId { get; set; }
+        public SuperPower SuperPower { get; set; }
+
     }
 }
